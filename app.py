9a7166fb3df9561228a23ac861c8877d71deb5d1
SVN Reddy <venkatasykam@yahoo.com>
2022-09-25 16:15:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,30 @@
-from flask import Flask, render_template
-import os
+from datetime import datetime
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def home():
-    users = [ 'Jenkins','GitHUB','Nexus' ]
-    return render_template('index.html', title='Welcome', members=users)
+def index():
+   print('Request for index page received')
+   return render_template('index.html')
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.route('/hello', methods=['POST'])
+def hello():
+   name = request.form.get('name')
+
+   if name:
+       print('Request for hello page received with name=%s' % name)
+       return render_template('hello.html', name = name)
+   else:
+       print('Request for hello page received with no name or blank name -- redirecting')
+       return redirect(url_for('index'))
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+if __name__ == '__main__':
+   app.run()
