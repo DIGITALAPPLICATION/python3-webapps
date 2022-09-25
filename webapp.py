4220486b60c4eb5713870359b3f61ef765fa83f9
SVN Reddy <venkatasykam@yahoo.com>
2022-09-25 15:04:51 +0530

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    name = 'DevOps'
+    name = 'Jenkins'
     return render_template('index.html', title='Welcome', username=name)
 
 app.run(host='0.0.0.0', port=81)
