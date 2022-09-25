e5638271497fb919f11dfc7adec2362ee380d615
SVN Reddy <venkatasykam@yahoo.com>
2022-09-25 15:09:37 +0530

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,12 +1,22 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 
+from flask import Flask, redirect, url_for, request
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/index')
-def index():
-    name = 'Jenkins'
-    return render_template('index.html', title='Welcome', username=name)
+@app.route('/dashboard/<name>')
+def dashboard(name):
+   return 'welcome %s' % name
 
-app.run(host='0.0.0.0', port=81)
+@app.route('/login',methods = ['POST', 'GET'])
+def login():
+   if request.method == 'POST':
+      user = request.form['name']
+      return redirect(url_for('dashboard',name = user))
+   else:
+      user = request.args.get('name')
+      return render_template('login.html')
+
+if __name__ == '__main__':
+   app.run(debug = True)
