2954e27eabc4975be161f93496148147872c6ab7
SVN Reddy <venkatasykam@yahoo.com>
2022-09-25 15:18:43 +0530

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,12 +1,14 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/index')
-def index():
-    users = [ 'Jenkins','GitHUB','Nexus' ]
-    return render_template('index.html', title='Welcome', members=users)
 
-app.run(host='0.0.0.0', port=81)
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
